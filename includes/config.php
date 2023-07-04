2664a590e2f6f9687a5a908458e1f6acd6cef5b2
NishmithaNayak13 <136427639+NishmithaNayak13@users.noreply.github.com>
2023-07-04 15:12:22 +0530

    Major Changes

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,4 @@
 <?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','Student');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+$dbh = mysqli_connect("localhost","root","","student");
+echo mysqli_connect_error();
 ?>
