74bdb3bee550712b23a2975f79c8058a83e647e6
NishmithaNayak13 <136427639+NishmithaNayak13@users.noreply.github.com>
2023-07-17 21:09:25 +0530

    db update

--- add-mentors.php
+++ add-mentors.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include('includes/config.php');
 if(!isset($_SESSION['UserName']))
 {
 	header("Location: index.php");
--- manage-mentors.php
+++ manage-mentors.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include('includes/config.php');
 if(!isset($_SESSION['UserName']))
 {
 	echo "<script>window.location='index.php';</script>";
